f3d093b7163ea2c40af18296f6c213c83586c029
Carlos Huamaní <carlos.hs.92@gmail.com>
2019-10-09 00:21:18 -0500

    feat: add upload service logic

--- lib/display_picture_screen.dart
+++ lib/display_picture_screen.dart
@@ -1,19 +1,46 @@
 import 'dart:io';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
+File file;
 // A widget that displays the picture taken by the user.
 class DisplayPictureScreen extends StatelessWidget {
   final String imagePath;
+  final String phpEndPoint = 'http://0.0.0.0/phpAPI/image.php';
 
   const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
+  void _upload() {
+    if (file == null) return;
+    String base64Image = base64Encode(file.readAsBytesSync());
+    String fileName = file.path.split("/").last;
 
+    http.post(phpEndPoint, body: {
+      "image": base64Image,
+      "name": fileName,
+    }).then((res) {
+      print(res.statusCode);
+    }).catchError((err) {
+      print(err);
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Display the Picture')),
       // The image is stored as a file on the device. Use the `Image.file`
       // constructor with the given path to display the image.
-      body: Image.file(File(imagePath)),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Image.file(File(imagePath)),
+          RaisedButton(
+            color: Colors.red,
+            onPressed: _upload,
+            child: Text("Evaluar Foto", style: TextStyle(fontSize: 20),),
+          ),
+        ],
+      )
     );
   }
 }
